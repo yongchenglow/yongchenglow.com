8fba7a12bc66e98adee6fa4c853c3f27a355c477
Yong Cheng Low <lowyongcheng@hotmail.com>
2022-01-24 00:19:44 +0800

    Config: Fix graphql build errors

--- pages/blog/2.tsx
+++ pages/blog/2.tsx
@@ -1,15 +1,6 @@
-import {
-  Box,
-  Container,
-  Card,
-  CardActions,
-  CardContent,
-  Typography,
-  Button,
-} from "@mui/material";
+import { Box, Container, Typography } from "@mui/material";
 import Footer from "../components/footer";
 import ResponsiveAppBar from "../components/appbar";
-import theme from "../../src/theme";
 
 const About = () => {
   return (
