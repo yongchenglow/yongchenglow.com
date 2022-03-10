d0e46d7c58063f7d689cbd1f54b5feea9edfcf40
Yong Cheng Low <lowyongcheng@hotmail.com>
2022-03-10 23:36:13 +0800

    Config: Add commit lint

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+const ERROR = 2;
+
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-empty': [ERROR, 'never'],
+    'type-empty': [ERROR, 'never'],
+    'type-case': [ERROR, 'never'],
+    'subject-case': [ERROR, 'never'],
+    'header-max-length': [2, 'always', 72],
+    'type-enum': [
+      2,
+      'always',
+      [
+        // Modules
+        'Init',
+        'Release',
+        'Housekeeping',
+        'Config',
+        'Blog',
+        'About',
+        'Index',
+      ],
+    ],
+  },
+};
