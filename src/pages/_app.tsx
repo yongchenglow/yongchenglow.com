830751eb5a4645c8e35b0b278f65af2e1a2834af
Yong Cheng Low <lowyongcheng@hotmail.com>
2022-05-21 22:10:21 +0800

    Config: Seperate head from app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,3 @@
-import Head from 'next/head';
 import { AppProps } from 'next/app';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -7,34 +6,16 @@ import '@fontsource/roboto/700.css';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import theme from '@/src/theme';
-import Script from 'next/script';
+import AppScript from './_scripts';
+import AppHead from './_head';
 import { NextPage } from 'next';
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
+      <AppHead />
       <ThemeProvider theme={theme}>
-        <Script
-          data-ad-client="ca-pub-5565145070747476"
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        />
-        <Script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-0YZD5J7B2T"
-        />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-0YZD5J7B2T');
-            `}
-        </Script>
+        <AppScript />
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
         <Component {...pageProps} />
