6a9af17aca4cf83fddff68fb82790750f7ee7477
Low Yong Cheng (YC) <lowyongcheng@hotmail.com>
2022-11-21 19:44:19 +0800

    Housekeeping: Upgrade libraries and remove redundant ones (#41)
    * Housekeeping: Upgrade libraries and remove redundant ones
    
    * Housekeeping: Adjust snapshots and linting

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,36 +1,9 @@
-import { InMemoryCache, gql } from '@apollo/client';
 import renderer from 'react-test-renderer';
-import { MockedProvider } from '@apollo/client/testing';
 import Index from '@/src/pages';
-
-const cache = new InMemoryCache();
-cache.writeQuery({
-  query: gql`
-    query Viewer {
-      viewer {
-        id
-        name
-        status
-      }
-    }
-  `,
-  data: {
-    viewer: {
-      __typename: 'User',
-      id: 'Baa',
-      name: 'Baa',
-      status: 'Healthy',
-    },
-  },
-});
 
 describe('Index', () => {
   it('renders the html we want', async () => {
-    const component = renderer.create(
-      <MockedProvider cache={cache}>
-        <Index />
-      </MockedProvider>,
-    );
+    const component = renderer.create(<Index />);
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
