ae02c7022205454854549fe1c8b312f7167b5b9e
Low Yong Cheng (YC) <lowyongcheng@hotmail.com>
2022-11-22 22:01:42 +0800

    Docker: Reduce docker build size (#45)
    * Housekeeping: Fix docker build issues
    
    * Docker: Adjust build to use standalone website
    
    * Docker: Remove unnecessary files from build

--- next.config.js
+++ next.config.js
@@ -2,4 +2,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer({ output: 'standalone' });
