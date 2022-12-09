dd72e7fb6a862b43a17df8de3d319dc96804ff8a
yong cheng low <lowyongcheng@hotmail.com>
2022-12-09 22:13:34 +0800

    Config: Add lang for html

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 const Document = () => {
   return (
-    <Html prefix="og: http://ogp.me/ns#">
+    <Html prefix="og: http://ogp.me/ns#" lang="en">
       <Head />
       <body>
         <Main />
