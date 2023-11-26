44753324fde659a69cb8ee8bd8df0e50d61b7d0e
Yong Cheng Low <lowyongcheng@hotmail.com>
2023-11-26 11:26:18 +0800

    Config: Update Libraries

--- src/pages/layouts/standard.tsx
+++ src/pages/layouts/standard.tsx
@@ -3,11 +3,7 @@ import Footer from '@/src/components/organisms/footer';
 import { Box, Container } from '@mui/material';
 import React from 'react';
 
-interface Props {
-  children?: React.ReactNode;
-}
-
-const StandardLayout: React.FunctionComponent<Props> = ({ children }) => {
+const StandardLayout = ({ children }): React.ReactElement => {
   return (
     <Box height="100vh" paddingTop={8} display="flex" flexDirection="column">
       <ResponsiveAppBar />
