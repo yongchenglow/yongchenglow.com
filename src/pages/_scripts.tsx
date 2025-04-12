46d2315567ce7c04bc87eade0c2212c51631692f
Yong Cheng Low <lowyongcheng@hotmail.com>
2025-04-13 02:13:59 +0800

    fix: adsense

--- src/pages/_scripts.tsx
+++ src/pages/_scripts.tsx
@@ -4,9 +4,9 @@ const AppScript = () => {
   return (
     <>
       <Script
-        data-ad-client={process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID}
         async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE_CLIENT_ID}`}
+        crossOrigin="anonymous"
       />
       <Script
         async
