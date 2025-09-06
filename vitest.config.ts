933d0adaff1ec4ae911588b8ec9daf086115d715
Yong Cheng Low <lowyongcheng@hotmail.com>
2025-09-06 13:37:11 +0800

    chore: update tsconfig for target

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { resolve } from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: ['./test/setup.ts'],
-	},
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, './'),
-		},
-	},
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./test/setup.ts"],
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./"),
+    },
+  },
 });
